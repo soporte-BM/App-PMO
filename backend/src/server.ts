73cde1bf865c1c3d8d0890f526516c62e2920f23
soporte-BM <soporte_ti@bmining.cl>
2026-03-25 15:38:01 -0300

    Content-Security-Policy

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -17,7 +17,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(helmet());
+app.use(helmet({
+    contentSecurityPolicy: false,
+}));
 app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
